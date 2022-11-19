bfa1747397f4211922e117239a93b468470532f6
Riyaz Ali <ra3411899@gmail.com>
2022-11-20 02:26:08 +0530

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='PROGRAM_3',
+   version='1.0',
+   description='A useful module',
+   author='Riyaz Ali',
+   author_email='ra3411899@gmail.com',
+   packages=['PROGRAM_3'],  #same as name
+   install_requires=['django'], #external packages as dependencies
+)
